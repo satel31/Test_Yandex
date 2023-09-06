255f493707a173aad1cbadf7834205ad68a824e2
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-06 23:16:28 +0300

    Changes vars

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 from src.pages import SearchPage
 
 URL = 'https://ya.ru/'
+search_word = 'Тензор'
+search_result = 'tensor.ru'
 
 def test_yandex_search(browser):
     main_page = SearchPage(browser, URL)
     main_page.open()
-    main_page.enter_word('Тензор')
+    main_page.enter_word(search_word)
     resulted_link = main_page.results()
-    assert 'tensor.ru' in resulted_link
+    assert search_result in resulted_link
