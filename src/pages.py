6cb349bf09688e715029ad76aa1a4c51f7d69574
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-07 00:12:38 +0300

    Added imagepage

--- src/pages.py
+++ src/pages.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-from src.locators import SearchLocators
+from src.locators import SearchLocators, ImagesLocators
 
 
 class BasePage:
@@ -11,15 +11,15 @@ class BasePage:
         self.driver = driver
         self.url = url
 
-    def find_element(self, key, time=10):
+    def find_element(self, key, time=120):
         return WebDriverWait(self.driver, time).until(ec.presence_of_element_located(key),
                                                       message=f'No key word {key}')
 
-    def find_elements(self, key, time=10):
+    def find_elements(self, key, time=120):
         return WebDriverWait(self.driver, time).until(ec.presence_of_all_elements_located(key),
                                                       message=f'No key word {key}')
 
-    def is_visible(self, key, time=10):
+    def is_visible(self, key, time=120):
         return WebDriverWait(self.driver, time).until(ec.visibility_of_element_located(key),
                                                       message=f'No key word {key}')
 
@@ -40,3 +40,15 @@ class SearchPage(BasePage):
         links = self.driver.find_elements(*SearchLocators.LINKS)
         link = links[0].get_attribute('href')
         return link
+
+class ImagePage(BasePage):
+    def menu(self):
+        # window_before = self.driver.window_handles[0]
+        self.find_element(SearchLocators.SEARCH_FIELD).click()
+        services = self.find_element(ImagesLocators.ALL_SERVICES)
+        services.click()
+        links = self.driver.find_elements(*ImagesLocators.IMAGES)
+        images_link = list(filter(lambda x: x.get_attribute('href') == 'https://ya.ru/images/', links))
+        images_link[0].click()
+
+
