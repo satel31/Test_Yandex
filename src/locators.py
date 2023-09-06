3a461ce0ce96c1ebacae7b6b19844a8e7177b972
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-07 00:12:21 +0300

    Added image locators

--- src/locators.py
+++ src/locators.py
@@ -5,3 +5,7 @@ class SearchLocators:
     SEARCH_FIELD = (By.ID, 'text')
     SUGGEST = (By.CSS_SELECTOR, 'div.mini-suggest__popup')
     LINKS = (By.CSS_SELECTOR, 'li div div a')
+
+class ImagesLocators:
+    ALL_SERVICES = (By.CLASS_NAME, 'services-suggest__list-item-more')
+    IMAGES = (By.CSS_SELECTOR, 'div div span a')
