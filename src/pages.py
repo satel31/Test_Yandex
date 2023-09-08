b2cdf980a86edbdcbda53f71baf0945ac677524f
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-09 02:07:16 +0300

    Added tests

--- src/pages.py
+++ src/pages.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -23,6 +25,10 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(ec.visibility_of_element_located(key),
                                                       message=f'No key word {key}')
 
+    def is_present(self, key, time=120):
+        return WebDriverWait(self.driver, time).until(ec.presence_of_element_located(key),
+                                                      message=f'No key word {key}')
+
     def open(self):
         return self.driver.get(self.url)
 
@@ -41,14 +47,34 @@ class SearchPage(BasePage):
         link = links[0].get_attribute('href')
         return link
 
+
 class ImagePage(BasePage):
     def menu(self):
-        # window_before = self.driver.window_handles[0]
         self.find_element(SearchLocators.SEARCH_FIELD).click()
         services = self.find_element(ImagesLocators.ALL_SERVICES)
         services.click()
         links = self.driver.find_elements(*ImagesLocators.IMAGES)
         images_link = list(filter(lambda x: x.get_attribute('href') == 'https://ya.ru/images/', links))
         images_link[0].click()
+        window_after = self.driver.window_handles[1]
+        self.driver.switch_to.window(window_after)
+        assert 'https://ya.ru/images/' in self.driver.current_url
+        time.sleep(2)
 
+    def choose_category(self):
+        category = self.driver.find_elements(*ImagesLocators.CATEGORY)
+        category_link = list(
+            filter(lambda x: x.get_attribute('href') and 'https://ya.ru/images/search' in x.get_attribute('href'),
+                   category))
+        category_name = category_link[0].text
+        category_link[0].click()
+        category_text = self.driver.find_element(*ImagesLocators.CATEGORY_TEXT)
+        assert category_text.get_attribute('value') == category_name
 
+    def picture(self):
+        time.sleep(2)
+        pictures = self.driver.find_elements(*ImagesLocators.PICTURE)
+        time.sleep(2)
+        pictures[0].click()
+        assert self.is_present(ImagesLocators.PICTURE_WINDOW)
+        time.sleep(2)
