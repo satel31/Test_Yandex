314040bcc8d68bc29e20683e4601d40f11c0c905
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-09 22:42:28 +0300

    Changed some locators

--- src/locators.py
+++ src/locators.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 
 class SearchLocators:
     SEARCH_FIELD = (By.ID, 'text')
-    SUGGEST = (By.CSS_SELECTOR, 'div.mini-suggest__popup')
-    LINKS = (By.CSS_SELECTOR, 'li div div a')
+    SUGGEST = (By.CSS_SELECTOR, '[class="mini-suggest__overlay mini-suggest__overlay_theme-color_dark mini-suggest__overlay_visible"]')
+    LINKS = (By.CSS_SELECTOR, '[class="Link Link_theme_normal OrganicTitle-Link organic__url link"]')
 
 class ImagesLocators:
     ALL_SERVICES = (By.CLASS_NAME, 'services-suggest__list-item-more')
     IMAGES = (By.CSS_SELECTOR, 'div div span a')
     CATEGORY = (By.CSS_SELECTOR, 'div div div div a')
     CATEGORY_TEXT = (By.CSS_SELECTOR, '[class="input__control mini-suggest__input"]')
-    PICTURE = (By.CSS_SELECTOR, 'body div div div div div div a')
+    PICTURE = (By.CSS_SELECTOR, '[class="serp-item__link"]')
     PICTURE_WINDOW = (By.CSS_SELECTOR, '[class="MMImageContainer"]')
+    OPENED_PICTURE = (By.CSS_SELECTOR, '[class="MMImage-Preview"]')
+    FORWARD_BUTTON = (By.CSS_SELECTOR, '.MediaViewer-ButtonNext.MediaViewer_theme_fiji-ButtonNext > i')
+    BACK_BUTTON = (By.CSS_SELECTOR, '.MediaViewer-ButtonPrev.MediaViewer_theme_fiji-ButtonPrev > i')
