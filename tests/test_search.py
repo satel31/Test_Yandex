9f7f6552a614f254cf71eaf9516f3029e9ba57ff
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-09 22:42:59 +0300

    Added msg for assert

--- tests/test_search.py
+++ tests/test_search.py
@@ -7,6 +7,7 @@ search_result = 'tensor.ru'
 def test_yandex_search(browser):
     main_page = SearchPage(browser, URL)
     main_page.open()
+    main_page.search_field()
     main_page.enter_word(search_word)
     resulted_link = main_page.results()
-    assert search_result in resulted_link
+    assert search_result in resulted_link, f"Первая ссылка не ведёт на сайт{search_result}"
