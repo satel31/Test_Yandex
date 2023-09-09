6d1596ea8476b9c8d9c34d090e285d1ad5602957
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-09 23:15:52 +0300

    Added logs

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,11 +1,18 @@
+from logs import logs
 from src.pages import ImagePage
 
 URL = 'https://ya.ru/'
 
-def test_yandex_images(browser):
-    main_page = ImagePage(browser, URL)
-    main_page.open()
-    main_page.menu()
-    main_page.choose_category()
-    main_page.picture()
 
+def test_yandex_images(browser):
+    logs(info='Тест начат')
+    try:
+        main_page = ImagePage(browser, URL)
+        main_page.open()
+        main_page.menu()
+        main_page.choose_category()
+        main_page.picture()
+    except Exception as e:
+        logs(error=e)
+    finally:
+        logs(info='Тест закончен')
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,4 @@
+from logs import logs
 from src.pages import SearchPage
 
 URL = 'https://ya.ru/'
@@ -5,9 +6,15 @@ search_word = 'Тензор'
 search_result = 'tensor.ru'
 
 def test_yandex_search(browser):
-    main_page = SearchPage(browser, URL)
-    main_page.open()
-    main_page.search_field()
-    main_page.enter_word(search_word)
-    resulted_link = main_page.results()
-    assert search_result in resulted_link, f"Первая ссылка не ведёт на сайт{search_result}"
+    logs(info='Тест начат')
+    try:
+        main_page = SearchPage(browser, URL)
+        main_page.open()
+        main_page.search_field()
+        main_page.enter_word(search_word)
+        resulted_link = main_page.results()
+        assert search_result in resulted_link, f"Первая ссылка не ведёт на сайт{search_result}"
+    except Exception as e:
+        logs(error=e)
+    finally:
+        logs(info='Тест закончен')
