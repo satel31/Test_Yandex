c1537614327fe9100b3e0019f2139a44ca3bf1f7
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-09 23:15:38 +0300

    Made logs

---
+++ logs.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def logs(error=None, info=None):
+    logger = logging.getLogger(__name__)
+    fileHandler = logging.FileHandler('logfile.log')
+    formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+    logger.setLevel(logging.INFO)
+    if info:
+        logger.info(f'Information message: {info}')
+    elif error:
+        logger.error(f'Error message: {error}')
